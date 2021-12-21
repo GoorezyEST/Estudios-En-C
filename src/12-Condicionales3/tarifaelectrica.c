a3940b23a0c2e927836b447934ee5ae6c9a8d96b
GoorezyEST <goorezyestudios@gmail.com>
2021-12-21 20:03:57 -0300

    Commit pasa solucionar Condicionales3

---
+++ src/12-Condicionales3/tarifaelectrica.c
@@ -0,0 +1,35 @@
+//Visualizar la tarifa de la luz segun el gasto de corriente electrica.
+//Para un gasto menor de 1.000KwxH
+//La tarifa es 1.2, entre 1.000y I.850KwxH es 1.0 y mayor de 1.850KwxH 0.9.
+
+
+#include <stdio.h>
+#define TARIFA1 1.2
+#define TARIFA2 1.0
+#define TARIFA3 0.9
+
+int maint(){
+
+	float gasto;
+	float tasa;
+
+	printf("Digite el total de gasto de energia: ");
+	scanf("%f",&gasto);
+
+	if (gasto < 1000){
+		tasa = TARIFA1;
+	}
+
+	if (gasto > 1000 && gasto < 1850){
+		tasa = TARIFA2;
+	}
+
+	if (gasto > 1850){
+		tasa = TARIFA3;
+	}
+
+	printf("La tasa a pagar es: %f",tasa);
+
+	return 0;
+
+}
