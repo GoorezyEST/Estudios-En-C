236dc6b0849a3a85593f71656edcf8cc61965c8c
GoorezyEST <goorezyestudios@gmail.com>
2021-12-21 15:42:50 -0300

    Termine de modificar areatrapecio y me pase a windows terminal

--- src/6-Operadores2/scr/areatrapecio.c
+++ src/6-Operadores2/scr/areatrapecio.c
@@ -7,17 +7,17 @@ int main(){
 	int base_M,base_m,area,altura;
 
 	printf("Digite la base mayor: ");
-	scanf("%i",&base_M);
+        scanf("%i",&base_M);
 
 	printf("Digite la base menor: ");
-	scanf("%i",&base_m);
+	//scanf("%i",&base_m);
 
 	printf("Digite la altura: ");
-	scanf("%i",&altura);
+	//scanf("%i",&altura);
 
-	area = ((base_M+base_m)*altura)/2;
+	//area = ((base_M+base_m)*altura)/2;
 
-	printf("\nEl area del trapecio es: %i",area);
+	//printf("\nEl area del trapecio es: %i",area);
 
 	return 0;
 }
