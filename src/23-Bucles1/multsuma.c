75698bfaf95de9f975fb89b1c34ce53a73e2de21
GoorezyEST <goorezyestudios@gmail.com>
2021-12-28 16:45:24 -0300

    Destrabe bucle 23

--- src/23-Bucles1/multsuma.c
+++ src/23-Bucles1/multsuma.c
@@ -1,3 +1,34 @@
-//Ingrese 5 numeros, si el ultimo numero es menor a 10 sumelos y si es mayor multipliquelos.
+//Ingrese 1 numero, si el numero supera 10 multiplique los 10 primeros numeros, si no sumelos.
 
+#include <stdio.h>
+
+int main(){
+	
+	int n,o;
+
+	printf("Ingrese un numero: ");
+	scanf("%i",&n);
+
+	while (n<1){
+		printf("\nEl numero no es correcto");
+		printf("\nIngrese un numero positivo: ");
+		scanf("%i",&n);
+	}
+
+	if (n < 10){
+		
+		o = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+		printf("\nEl resultado es: %i",o);
+
+	}
+
+	else {
+		o = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10;
+		printf("\nEl resultado es: %i",o);
+
+	}
+
+	return 0;
+
+}
 
