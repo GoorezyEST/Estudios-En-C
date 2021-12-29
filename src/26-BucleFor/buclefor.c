8e295824b2888c41a8bb0505a445fdc63d92ecc0
GoorezyEST <goorezyestudios@gmail.com>
2021-12-29 13:57:22 -0300

    El bucle FOR

---
+++ src/26-BucleFor/buclefor.c
@@ -0,0 +1,23 @@
+// El bucle FOR:
+//   Sintaxis:
+//
+//             for(inicializacion; condicion; incremento){
+//             			instrucciones;
+//             			...
+//             			}
+//
+// Mostrar los diez primeros numeros
+
+#include <stdio.h>
+
+int main(){
+
+	int i;
+
+	for (i=1; i <= 10; i++){
+		printf("%i.\n",i);
+	}
+
+	return 0;
+
+}
